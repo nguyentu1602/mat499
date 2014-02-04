17e9523e0226266d0551025e5d49110ce8b83236
nguyentu1602 <cuong.nguyentumanh@gmail.com>
2014-02-04 00:35:18 -0600

    Update on parallel computing

--- as-caida.R
+++ as-caida.R
@@ -1,5 +1,18 @@
 install.packages('multicore')
+install.packages('foreach')
+install.packages('biglm')
+install.packages('bigmemory')
+install.packages('doParallel')
+install.packages('parallel')
+
+
+
+require(foreach)
 require(multicore)
+require(biglm)
+require(bigmemory)
+require(doParallel)
+
 
 ### hash() and unHash() are functions to create hashCode
 
@@ -20,18 +33,17 @@ unHash <- function (z) {
 } #unHash
 
 # Testing hash() and unHash()
-# Note: it crashes when we have (10^6, 10^6)
-v1 <- c(1:10000000)
-v2 <- seq(from=10000000,to=1,by=-1)
-v2
-v3 <- c(1:10000000)
+# Note: it gives wrong round-off when we have (10^8, 10^8), but anything less works 
+# v1 <- c(1:10000000)
+# v2 <- seq(from=10000000,to=1,by=-1)
+# v2
+# v3 <- c(1:10000000)
+# 
+# for (i in 1:10000000) {
+#   temp = unHash(hash(v1[i], v2[i])) - c(v1[i], v2[i])
+#   v3[i] = !((temp[1] == 0) && (temp[2] == 0))
+# }
 
-for (i in 1:10000000) {
-  temp = unHash(hash(v1[i], v2[i])) - c(v1[i], v2[i])
-  v3[i] = !((temp[1] == 0) && (temp[2] == 0))
-}
-  
-unHash(hash(10000000,10000000))
 
 ### Load all the files at once:
 
@@ -50,8 +62,6 @@ unHash(hash(10000000,10000000))
 # names(dataFiles[[i]]) = c('fromNode','toNode', 'weight') 
 } # The dataFiles list is now cleaned and suitable for our purpose
 
-# t1 <- dataFiles[[1]]
-
 ### Saving the data;
 save(dataFiles,file="as-caida-cleaned.R")
 # load('/Users/cuongnguyen/Dropbox/00.Class/0.S14/MAT499/as-caida/as-caida-cleaned.R')
@@ -63,10 +73,9 @@ require(igraph)
 graphList <- list()
 graphList[[1]] <- graph.data.frame(dataFiles[[1]], directed = TRUE)
 graphList[[1]]
-plot(graphList[[1]])
+
 rm(list=ls())
 
-system.time(graphList <- lapply(dataFiles, graph.data.frame))
 system.time(graphList <- mclapply(dataFiles, graph.data.frame))
 
 # Get all edges list
@@ -78,28 +87,66 @@ f1 <- function (mat) {
   return( cbind (as.numeric(mat[,1]), as.numeric(mat[,2])));}
 
 edgeList <- list()
-
 edgeList <- mclapply(edgeListStr, f1)
-
-
-#system.time(edgeList <- mclapply(edgeList, data.frame))
-
-
-system.time( mclapply(edgeList, function (frame) { 
-  cbind(frame, matrix(nrow=length(frame[,1]),ncol=3));      
-        }
-  ))
-
-#rm(edgeList)
 edgeList[[1]]
-         
-dim(edgeList[[122]])
-length(edgeList)
+
+# Created a list of 122 vectors, each containing hashcodes of all the edges
+
+ID <- list()
+ID <- mclapply(edgeList, function (mat) {
+  v <- vector(mode="numeric", length=length(mat[,1])); 
+  for (i in 1:length(mat[,1])) {
+    v[i] = hash(mat[i,1], mat[i,2])};
+  return (v);})
+
+# Created a list of 122-1 vectors of response variable Y: was it in the next graph
+Y <- list()
+percentSurvive <- vector(length=121)
+
+for (i  in 1:(length(ID) - 1)) {         
+  Y[[i]] = (ID[[i+1]] %in% ID[[i]]);
+  percentSurvive[i] = sum(Y[[i]])/length(Y[[i]]);
+}
+
+# A graph showing survivor rate over time
+plot(percentSurvive)
+# Very interesting that the survival date for 20070917 is only 35% - I might have to left out 8 last graphs
+
+### Create the feature matrix. This is the big one
+X <- list()
+
+#c1 <- makeCluster(3)
+nodes <- detectCores()
+c1 <- makeCluster(nodes)
+registerDoParallel(c1)
+
+system.time (foreach (i = 1:20)  %do% {
+  X[[i]] = matrix(data=NA,nrow=length(ID[[20+i]]),ncol=100);
+} 
+)
+
+
+system.time (for (i in 1:10) {
+  X[[i]] = matrix(data=NA,nrow=length(ID[[20]]),ncol=200);
+} 
+)
+
+
+X[[1]]
+
+
+
+
+
+
+
 
 
 G3.e <- cbind(G3.e, matrix(nrow=length(G3.e[,1]),ncol=27))
 
+degree(graph=graphList[[1]], v="23231", mode ="in")
 
+a
 
 
 
